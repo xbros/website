7c57ff2d4e261658644e7f582e080ce05ef7c0f5
SimonTod <simon.todeschini@viacesi.fr>
2016-02-03 18:37:47 +0100

    test php

---
+++ intru.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>prositbalise</title>
+</head>
+
+<body>
+<h1>Vous avez répondu au petit jeu "trouvez l'intru"</h1>
+
+
+<a href="index.html">revenir à la page précédente</a>
+</body>
+</html>
