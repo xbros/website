9c1e033d65c8008308b922ddb615cf8079f62656
SimonTod <simon.todeschini@viacesi.fr>
2016-02-18 20:01:43 +0100

    test ip

--- bdd/sql_write-new-action.php
+++ bdd/sql_write-new-action.php
@@ -6,30 +6,44 @@
  * Time: 18:24
  */
 
-require_once __DIR__ . '/connection.php';
+function getIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (! empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+echo $ip;
+
+/*require_once __DIR__ . '/connection.php';
 $bdd = new DB_CONNECT();
 
 
 if($_GET['type'] == "" && $_GET['son'] == "")
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."')";
 }
 elseif($_GET['type'] == "")
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['son']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, son) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['son']."')";
 }
 elseif($_GET['son'] == "")
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."')";
 }
 else
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."', '".$_GET['son']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."', '".$_GET['son']."')";
 }
 
 
 
 
 $res_users = mysql_query($stmt_add_action) or die(mysql_error());
-
+*/
 ?>
