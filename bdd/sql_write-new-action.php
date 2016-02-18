10afd8f38f4e900e7e28b3368ac06c52e57e8241
SimonTod <simon.todeschini@viacesi.fr>
2016-02-18 19:13:19 +0100

    test bdd

--- bdd/sql_write-new-action.php
+++ bdd/sql_write-new-action.php
@@ -9,17 +9,18 @@
 require_once __DIR__ . '/connection.php';
 $bdd = new DB_CONNECT();
 
-if($_GET['type'] == "")
+
+if($_GET['type'] == "" && $GET['son'] == "")
+{
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."')";
+}
+elseif($_GET['type'] == "")
 {
     $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['son']."')";
 }
-elseif($GET['son'] == "")
+elseif($_GET['son'] == "")
 {
     $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."')";
-}
-elseif($_GET['type'] == "" && $GET['son'] == "")
-{
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."')";
 }
 else
 {
