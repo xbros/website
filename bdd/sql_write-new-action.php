2aa9c52327c3697234fa35924b3b9f083845fa8b
SimonTod <simon.todeschini@viacesi.fr>
2016-02-18 20:11:50 +0100

    test ip

--- bdd/sql_write-new-action.php
+++ bdd/sql_write-new-action.php
@@ -27,38 +27,34 @@ function ip_details($ip) {
 
 $ip = getIP();
 
+
 $ipDetails = ip_details($ip);
 
-echo $ip;
-echo "</br>";
-echo $ipDetails->country;
-echo "</br>";
-echo $ipDetails->city;
 
-/*require_once __DIR__ . '/connection.php';
+require_once __DIR__ . '/connection.php';
 $bdd = new DB_CONNECT();
 
 
 if($_GET['type'] == "" && $_GET['son'] == "")
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action) VALUES ('".$ip."', '".$ipDetails->country."', NOW(), '".$_GET['action']."')";
 }
 elseif($_GET['type'] == "")
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, son) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['son']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, son) VALUES ('".$ip."', '".$ipDetails->country."', NOW(), '".$_GET['action']."', '".$_GET['son']."')";
 }
 elseif($_GET['son'] == "")
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type) VALUES ('".$ip."', '".$ipDetails->country."', NOW(), '".$_GET['action']."', '".$_GET['type']."')";
 }
 else
 {
-    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".getIP()."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."', '".$_GET['son']."')";
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".$ip."', '".$ipDetails->country."', NOW(), '".$_GET['action']."', '".$_GET['type']."', '".$_GET['son']."')";
 }
 
 
 
 
 $res_users = mysql_query($stmt_add_action) or die(mysql_error());
-*/
+
 ?>
