e4c24cf8429034658cf7241434a223a9f12f1560
SimonTod <simon.todeschini@viacesi.fr>
2016-02-18 15:57:49 +0100

    Create simon.php

---
+++ simon.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simon</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <?php include("include/in_header.html"); ?>
+</head>
+<body>
+<?php include("include/before_body.html"); ?>
+
+
+<h1>Simon</h1>
+
+<a class="btn btn-default" href="simon-music.php"><i class="fa fa-headphones"></i> Music</a>
+
+
+<?php include("include/after_body.html"); ?>
+</body>
+</html>
