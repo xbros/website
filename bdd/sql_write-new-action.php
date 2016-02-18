f79b37bce0a3f8ce4e66f2cea17e6a2adb43500e
SimonTod <simon.todeschini@viacesi.fr>
2016-02-18 19:01:22 +0100

    test bdd

--- bdd/sql_write-new-action.php
+++ bdd/sql_write-new-action.php
@@ -9,7 +9,26 @@
 require_once __DIR__ . '/connection.php';
 $bdd = new DB_CONNECT();
 
-$stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."', '".$_GET['son']."')";
+if($_GET['type'] == "")
+{
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['son']."')";
+}
+elseif($GET['son'] == "")
+{
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."')";
+}
+elseif($_GET['type'] == "" && $GET['son'] == "")
+{
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."')";
+}
+else
+{
+    $stmt_add_action = "INSERT INTO musicStats (ip, pays, date, action, type, son) VALUES ('".$_GET['ip']."', '".$_GET['pays']."', NOW(), '".$_GET['action']."', '".$_GET['type']."', '".$_GET['son']."')";
+}
+
+
+
+
 $res_users = mysql_query($stmt_add_action) or die(mysql_error());
 
 ?>
---
+++ javascripts/write-action-bdd.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Simon on 18/02/2016.
+ */
+var ajaxMysqlRequest = function(lienPhp, returnElement)
+{
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById(returnElement).innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET",lienPhp,true);
+    xmlhttp.send();
+};
