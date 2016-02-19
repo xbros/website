d0b6ac81721c252263e688b49bbe9b6687d01e07
SimonTod <simon.todeschini@viacesi.fr>
2016-02-19 15:38:20 +0100

    test bootstrap

--- simon.php
+++ simon.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Simon</title>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="lib/jquery-1.11.0/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="lib/bootstrap-3.3.1/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="lib/bootstrap-3.3.1/js/bootstrap.min.js"></script>
+    <script src="lib/bootstrap-3.3.1/shim/html5shiv.min.js"></script>
+    <script src="lib/bootstrap-3.3.1/shim/respond.min.js"></script>
+
 
     <?php include("include/in_header.html"); ?>
     <link rel="stylesheet" href="simon.css">
