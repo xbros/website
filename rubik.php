a51446f8f11cf143a7fb3e0fa215aed805495eca
todeschi <adrien.todeschini@inria.fr>
2016-02-27 00:58:25 +0100

    keyboard ctrls

--- rubik.php
+++ rubik.php
@@ -18,8 +18,14 @@
         <?php include("include/before_body.html"); ?>
 
         <h1>Rubik's cube</h1>
-        <div id='my-cube' class="rb-cube" style="text-align:center;"></div>
-        <div id='my-ctrl' class="rb-ctrl" style="text-align:center;"></div>
+        <p>
+            Use the buttons below or the keyboard to move the cube.
+            Clockwise 90°: [F]ront, [B]ack, [L]eft, [R]ight, [U]p, [D]own.
+            Anti-clockwise 90°: Shift-[key].
+            180°: Shift-Ctrl-[key].
+        </p>
+        <div id='my-cube' class="rb-cube"></div>
+        <div id='my-ctrl' class="rb-ctrl"></div>
         <div class="rb-moves">
             <button onclick="document.getElementById('my-moves').classList.toggle('hide')">Moves</button>
             <p id='my-moves'></p>
